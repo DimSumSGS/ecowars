c9294a1c321e9546409524ee76d07747af3d3cf5
Vincent <xrocxion@gmail.com>
2016-09-29 16:41:16 -0700

    homepage pull

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,27 +41,6 @@ $(document).ready(function(){
         
     });
  
-
-
-<<<<<<< HEAD
-  $( ".clicker" )
-    .mouseup(function() {
-      $( this ).removeClass( "pressed" );
-    })
-    .mousedown(function() {
-      $( this ).addClass( "pressed" );
-    });
-
-  $( ".buy_trees")
-    .click(function(){
-      $( this ).append("plus 1");
-    });
-
-  for (var tree = 1; tree < 9; tree++) {
-    console.log(tree);
-  }
-});
-=======
 });
 
 function modify_qty(val) {
