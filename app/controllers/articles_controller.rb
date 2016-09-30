339f1ea083e4b6002c8486a7d21feee1947f80f3
DimSumSGS <jsung2000@gmail.com>
2016-09-29 17:33:35 -0700

    commit2'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,22 +9,15 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 	def create
- 		@article = Article.new(params.require(:article).permit(:title, :text))
+  		@article = Article.new(article_params)
  
   		@article.save
   		redirect_to @article
 	end
- 	
- 	def destroy
-  		@article = Article.find(params[:id])
-  		@article.destroy
-  		redirect_to articles_path
-	end
-	
+ 
 	private
   		def article_params
     		params.require(:article).permit(:title, :text)
   		end
-
- 
 end
+
