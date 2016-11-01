6ee372ce46835b1265a5d7901609922f54fcb938
DimSumSGS <jsung2000@gmail.com>
2016-10-31 19:45:07 -0700

    Progress bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,6 +63,9 @@ $(document).ready(function(){
       $( this ).addClass( "pressed" );
     });
 });
+
+/* Notes that the game is on. 1 means on, 0 means game over*/
+var game = 1
 /* Tree + Resources timer */
 
 
@@ -70,30 +73,79 @@ var treeqty = 0;
 function buytrees()
 {
   treeqty = treeqty + 1;
-  document.getElementById("treeqtydisplay").innerHTML = treeqty
+  document.getElementById("treeqtydisplay").innerHTML = treeqty;
   return treeqty
 }
 
 function selltrees()
 {
+  if (treeqty > 0) {
   treeqty = treeqty - 1;
-  document.getElementById("treeqtydisplay").innerHTML = treeqty
+  document.getElementById("treeqtydisplay").innerHTML = treeqty;
   return treeqty
+ }
 }
 
 
 
-var counter=setInterval(resources, 5);
-var count = 0
-function resources()
-{
-  count=count+treeqty;
-  if (count > 100000)
-  {
-      clearInterval(counter);
-      return;
-  }
+var resourcecounter=setInterval(resources, 1000);
+var resourcecount = 0
+
+function resources() {
+    resourcecount=resourcecount+treeqty;
+    if (resourcecount > 10000)
+    {
+        clearInterval(resourcecounter);
+        return;
+    }
   
-  document.getElementById("resources").innerHTML=count + " Resources";
+    document.getElementById("resources").innerHTML=resourcecount + " Resources";
+  
 }
+
+/* Waves */
+/*variables*/
+var wavecounter = setInterval(waves, 10000) /* Sets the waves to happen every 30 seconds */
+var wavecount = 0;
+
+
+function waves() {
+  wavecount = wavecount + 1;
+  var treeloss = Math.pow(wavecount, 2); /* Squares the wavecount (e.g 10 x 10 = 100 trees lost) */
+  var resourceloss = Math.pow(wavecount, 3); /*Sets the resource loss to be the wave level^2 */
+  if (resourcecount - resourceloss >=0) {
+  resourcecount = resourcecount - resourceloss;
+  }
+  else {
+    resourcecount = 0
+  }
+  if (treeqty - treeloss >= 0){
+  treeqty = treeqty - treeloss;
+  }
+  else {
+    treeqty = 0
+  }
+  document.getElementById("waves").innerHTML= " Wavecount: " + wavecount + " Resource Loss: " + resourceloss + " Tree loss: " + treeloss;
+  document.getElementById("treeqtydisplay").innerHTML = treeqty;
+  return;
+}
+
+// var barcounter = setInterval(increase, 1000);
+// // /*Progress bar*/
+// var percent= 0; 
+// var progress = document.getElementById("progress"); 
+// function increase(){ 
+//   console.log(resourcecount);
+//   percent = (resourcecount/10000)*100;
+//   if(percent>=100) {
+//     percent = 100;
+//     progress.style.width = percent + "%";
+//   }
+//   else {
+//     progress.style.width = percent + "%";
+//   };     
+// } 
+
+
+
 
